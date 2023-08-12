492b8fde5a59d8dd7a09172b4f6ddf223d8aeb3c
leemoraes1 <tad-leticiamoraes@ucpparana.edu.br>
2023-08-11 21:20:13 -0300

    beta

---
+++ atividade.py
@@ -0,0 +1,16 @@
+nome = ('leticia')
+sobrenome = ('da silva moraes')
+idade = ('19')
+peso = ('69kg')
+altura = ('1.60')
+
+
+print (nome, sobrenome)
+print (idade)
+print (peso)
+print (altura)
+
+if idade >= '18':
+    print ('maior de idade!')
+else:
+    print ('menor de idade!')
